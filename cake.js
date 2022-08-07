7207672abec9e11bdeabf6049e6cc4896dede65f
Rokibul-bde <mdrokibulislamcmt176@gmail.com>
2022-08-08 02:32:21 +0600

    cake.js added

---
+++ cake.js
@@ -0,0 +1,12 @@
+const jim = 80;
+const bela = 99;
+const chiku = 98;
+
+
+if(jim > bela && jim > chiku){
+    console.log('Jim will be winner');
+}else if(bela > jim && bela > chiku){
+    console.log('Bela will be winner');
+}else{
+    console.log('Chiku will be winner')
+}
