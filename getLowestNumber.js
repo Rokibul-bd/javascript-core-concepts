1269fc866e981c04cd7685f84cf7e3a0673ed068
Rokibul-bde <mdrokibulislamcmt176@gmail.com>
2022-08-07 18:18:19 +0600

    added getLowestNumberFunction

---
+++ getLowestNumber.js
@@ -0,0 +1,13 @@
+function getLowestNumber(numbers){
+    let lowestNumber = numbers[0];
+    for(let i = 0; i < numbers.length; i++){
+        if(lowestNumber > numbers[i]){
+            lowestNumber = numbers[i];
+        }
+    }
+    return lowestNumber;
+}
+
+const height = [150,190,250,140,160,130,245,222];
+
+console.log(getLowestNumber(height))
